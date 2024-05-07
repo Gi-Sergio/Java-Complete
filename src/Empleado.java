67655d201bac38d55e50507cdffcbb649284e0ad
Sergio <sermorasu123@gmail.com>
2024-05-07 08:02:27 -0500

    Polimorfismo - Implementación en Clase Empleado por [Sergio]

--- src/Empleado.java
+++ src/Empleado.java
@@ -2,6 +2,7 @@ public class Empleado extends Persona implements Trabajador{
     public Empleado(String nombre, Integer edad, Integer fechaNacimiento) {
         super(nombre, edad, fechaNacimiento);
     }
-
-
+    public void realizarTarea(){
+        System.out.println("Realizando tarea Laboral");
+    }
 }
