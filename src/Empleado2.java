889f26a79492f34bb6efe89f533826032736e1e3
Sergio <sermorasu123@gmail.com>
2024-05-07 08:24:12 -0500

    Herencia y Polimorfismo - Implementación de Clase Empleado por [Sergio]

---
+++ src/Empleado2.java
@@ -0,0 +1,25 @@
+public class Empleado2 {
+    private String nombre;
+    private double salario;
+
+    public Empleado2(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
